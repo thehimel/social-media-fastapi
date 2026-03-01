349068961932234ef4b525bd757d86656f0d4a2c
Himel Das <60639301+thehimel@users.noreply.github.com>
2026-03-01 19:22:20 +0100

    feat: add create_post endpoint with request body handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -10,3 +11,12 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+
+@app.post("/create")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": {
+        "title": payload["title"],
+        "content": payload["content"],
+    }}
